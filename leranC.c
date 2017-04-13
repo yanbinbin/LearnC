7c853074cb7c808201a239282172d5384011b868
严彬彬 <424410522@qq.com>
2017-04-13 23:10:38 +0800

    C语言学习：extern的意思 extern声明的变量或者函数，对于其它文件可见

--- leranC.c
+++ leranC.c
@@ -9,6 +9,8 @@ extern int b;
 
 static int static_a = 5; // 全局变量
 
+int count;
+extern void write_extern(void);
 // 函数申明
 void func(void);
 int main(void){
@@ -55,6 +57,12 @@ int main(void){
 	{
 		func();
 	}
+
+	// 4.extern 存储类
+	//   提供一个全局变量的引用，全局变量对所有的程序文件都是可见的。使用‘extern’时，对于无法初始化的变量，会把变量名指向一个之前定义过的存储位置
+	//   extern可以在另一个文件中声明一个全局变量或函数
+	count = 5;
+	write_extern();
 
 	return 1;
 }
